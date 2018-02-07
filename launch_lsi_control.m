c1a6ecd3c5b11679d8627d0739a6b09ffcf63fd4
Ryan-Miyakawa <ryan@hooktheory.com>
2018-02-06 17:37:06 -0800

    added dmi and wafer dose sensor

--- launch_lsi_control.m
+++ launch_lsi_control.m
@@ -1,11 +1,25 @@
-
-
-purge
 % delete timers:
-delete(timerfind)
+if exist('purge', 'file')
+    purge;
+end
 
-cJavaLibPath = pwd;
-app = lsicontrol.ui.LSI_Control('cJavaLibPath', cJavaLibPath);
+% micpath:
+% First just get mic.Utils so we can have genpath_exclude:
+cMicPath = fullfile(fileparts(mfilename('fullpath')), '..', '..', 'cnanders', 'matlab-instrument-control', 'src');
+addpath(cMicPath);
+addpath(mic.Utils.genpath_exclude(cMicPath, {'\+mic'}));
+
+% Hardware path:
+cBL12Path = fullfile(fileparts(mfilename('fullpath')), '..', '..', '..', '..', 'src');
+addpath(cBL12Path);
+% micpath:
+
+hardware = bl12014.Hardware();
+
+
+app = lsicontrol.ui.LSI_Control('hardware', hardware);
+
+
 
 %%
 app.build()
